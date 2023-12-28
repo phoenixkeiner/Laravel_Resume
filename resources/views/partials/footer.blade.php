5566f05f10c6409639a9e616988666a688bad0a6
Phoenix Keiner <phoenix-keiner@annies_publishing.com>
2023-12-28 17:04:01 -0500

    composer update

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -4,12 +4,12 @@
                     <div class="justify-center mt-16 px-0 sm:items-center sm:justify-between">
                         <a href="/">
                             <button type="button" class="text-black font-bold py-2 px-4 rounded bg-gradient-to-r from-emerald-300 to-sky-200 hover:from-rose-600 hover:to-yellow-200 dark:from-rose-800 dark:to-yellow-400 dark:hover:from-emerald-300 dark:hover:to-sky-400 ">
-                            return home
+                            Return Home
                             </button>
                         </a>
                     </div>
+                </div>
             </div>
         </div>
-    </div>
     </body>
 </html>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,13 +1,14 @@
-
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Phoenix Keiner's Resume!</title>
-        <!-- icon image from laravel's website -->
-        <link rel="icon" type="image/png" sizes="32x32" href="/img/laravelicon.png">
-        <!-- links style sheet from publix/css/app.css -->
-        <link rel="stylesheet" href="css/app.css">
-    </head>
+    <title>Phoenix Keiner's Resume!</title>
+
+    <!-- Favicon from Laravel's website -->
+    <link rel="icon" type="image/png" sizes="32x32" href="/img/laravelicon.png">
+
+    <!-- Link to the stylesheet (check the path) -->
+    <link rel="stylesheet" href="css/app.css">
+</head>
