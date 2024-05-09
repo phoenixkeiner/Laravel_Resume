d3f20667c0403452aaf29498771b452d24466874
Phoenix Keiner <phoenix-keiner@annies_publishing.com>
2024-05-09 11:51:10 -0400

    composer updates

--- resources/views/resume.blade.php
+++ resources/views/resume.blade.php
@@ -4,29 +4,20 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <title>Phoenix Keiner's Resume!</title>
-    <!-- icon image from laravel's website -->
     <link rel="icon" type="image/png" sizes="32x32" href="/img/laravelicon.png">
     <!-- links style sheet from publix/css/app.css -->
     <link rel="stylesheet" href="css/app.css">
 </head>
-
-<body
-    class="antialiased w-full flex justify-center selection:bg-fuchsia-300 selection:text-fuchsia-900
-                bg-gradient-to-br from-blue-300
-                dark:bg-gradient-to-br dark:from-blue-400">
+<body class="antialiased w-full flex justify-center bg-gradient-to-br from-blue-300 dark:from-blue-400">
     <div role="resume list" class="mx-auto p-6 lg:p-8">
-        <h2 class="pt-8 text-xl text-stone-700 mb-5">Timeline of my work!</h2>
+        <h2 class="pt-8 text-xl text-stone-700 mb-5">Timeline of My Work</h2>
         <a href="https://www.linkedin.com/in/phoenix-keiner?utm_source=website&utm_medium=resume&utm_campaign=laravel"
-            class="inline-flex items-center px-4 py-2 text-sm font-medium text-stone-900 bg-white border border-stone-200 rounded-lg hover:bg-stone-100 hover:text-blue-700
-            focus:z-10 focus:ring-4 focus:outline-none focus:ring-stone-200 focus:text-blue-700 dark:bg-stone-800 dark:text-stone-400 dark:border-stone-600
-            dark:hover:text-white dark:hover:bg-stone-700 dark:focus:ring-stone-700">LinkedIn
+            class="inline-flex items-center px-4 py-2 text-sm font-medium text-stone-900 bg-white border border-stone-200 rounded-lg hover:bg-stone-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-stone-200 focus:text-blue-700 dark:bg-stone-800 dark:text-stone-400 dark:border-stone-600 dark:hover:text-white dark:hover:bg-stone-700 dark:focus:ring-stone-700">LinkedIn
             <svg class="w-3 h-3 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd"
-                    d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
-                    clip-rule="evenodd"></path>
-            </svg></a>
+                <path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+            </svg>
+        </a>
 
         <ol class="relative border-l border-stone-200 dark:border-stone-700 py-4">
             <li class="mb-10 ml-4">
@@ -141,9 +132,7 @@
 
         <!-- Download button for my resume -->
         <a href="pdf/resume.pdf" download="resume.pdf">
-            <button
-                class="inline-flex items-center px-4 py-2 text-sm font-medium text-stone-900 bg-white border border-stone-200 rounded-lg hover:bg-stone-100 hover:text-blue-700 focus:z-10
-                    focus:ring-4 focus:outline-none focus:ring-stone-200 focus:text-blue-700 dark:bg-stone-800 dark:text-stone-400 dark:border-stone-600 dark:hover:text-white dark:hover:bg-stone-700 dark:focus:ring-stone-700">
+            <button class="inline-flex items-center px-4 py-2 text-sm font-medium text-stone-900 bg-white border border-stone-200 rounded-lg hover:bg-stone-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-stone-200 focus:text-blue-700 dark:bg-stone-800 dark:text-stone-400 dark:border-stone-600 dark:hover:text-white dark:hover:bg-stone-700 dark:focus:ring-stone-700">
                 <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                     <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
                 </svg>
